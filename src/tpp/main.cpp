a49a9f5bbccdc67295924ae9e3e1890800c11873
3dhater <artembasov@outlook.com>
2022-05-07 06:51:48 +0300

    project

---
+++ src/tpp/main.cpp
@@ -0,0 +1,23 @@
+﻿#include "Application.h"
+#include "framework/mgf.h"
+
+#include <Windows.h>
+
+#ifdef MG_DEBUG
+#pragma comment(lib, "mgfd.lib")
+#else
+#pragma comment(lib, "mgf.lib")
+#endif
+
+int main()
+{
+	Application* app = 0;
+	app = new Application;
+	if (app->Init())
+	{
+		app->Run();
+		delete app;
+	}
+	return 0;
+}
+
